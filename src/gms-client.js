4b2a94b6838bd2991eb109eb4e7c5e010db83353
felixdrp <felixdrp@gmail.com>
2016-02-02 12:28:49 +0000

    Added react-router-redux module.

--- src/gms-client.js
+++ src/gms-client.js
@@ -1,41 +1,50 @@
+// *** Load react and react-dom ***
 import React from 'react'
 import { render } from 'react-dom'
 
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-// import App from './containers/App'
-import App from './components/app-component'
-
-// import generalStore from './reducers/reducer-1'
-import counter from './reducers/reducer-1'
-// import counter from './reducers/reducer-1'
-
+// *** Load react-router ***
 import { browserHistory, Router, Route, IndexRoute, Link } from 'react-router'
 import { match, RoutingContext } from 'react-router'
 import Routes from './routes';
 import { createHistory } from 'history';
 
-// Grab the state from a global injected into server-generated HTML
+// *** Load redux ***
+import { applyMiddleware, compose, createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { syncHistory, routeReducer } from 'react-router-redux'
+
+// *** Load store reducers ***
+// import generalStore from './reducers/reducer-1'
+import counter from './reducers/reducer-1'
+
+// The initial state from server-generated HTML
+// have a look to server code.
 const initialState = window.__INITIAL_STATE__
 
-// Create Redux store with initial state
-// const store = createStore(counterApp, initialState)
-var history = createHistory();
-console.log(history)
-history.push(initialState.location)
-const store = createStore(counter);
-const routes = Routes( browserHistory );
+// https://github.com/rackt/history/blob/master/docs/GettingStarted.md
+const history = createHistory()
 
-console.log(routes)
-console.log(history)
+// https://github.com/rackt/react-router-redux/blob/master/examples/basic/app.js
+const middleware = syncHistory(history)
+const reducer = combineReducers({
+  ...counter,
+  routing: routeReducer
+})
 
-match({ location: initialState.location, routes }, (error, redirectLocation, renderProps) => {
-  render(
-    (
-      <Provider store={store}>
-        <RoutingContext {...renderProps} location={location} />
-      </Provider>
-    ),
-    document.getElementById('root')
-  )
-});
+// // Create Redux store with initial state
+// // const store = createStore(counterApp, initialState)
+
+const finalCreateStore = compose(
+  applyMiddleware(middleware)
+  // DevTools.instrument()
+  //
+)(createStore)
+const store = finalCreateStore(reducer)
+middleware.listenForReplays(store)
+
+render(
+  <Provider store={store}>
+    {Routes( history )}
+  </Provider>,
+  document.getElementById('root')
+)
