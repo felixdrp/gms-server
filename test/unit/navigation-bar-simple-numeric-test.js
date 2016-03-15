6c9d50de8449a3b5e0ce0f675c6eab112a1ac750
felixdrp <felixdrp@gmail.com>
2016-03-15 16:56:04 +0000

    Added unit testing.

---
+++ test/unit/navigation-bar-simple-numeric-test.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+'use strict';
+
+jest.unmock();
+// jest.unmock('../CheckboxWithLabel');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+// import CheckboxWithLabel from '../../CheckboxWithLabel';
+
+var Compo = (props) => (
+ <div id="maincontainer">
+   hello
+ </div>
+);
+
+describe('Compo', () => {
+
+  it('changes the text after click', () => {
+    // Render a checkbox with label in the document
+    const checkbox = TestUtils.renderIntoDocument(
+      <Compo labelOn="On" labelOff="Off" />
+    );
+
+    const checkboxNode = ReactDOM.findDOMNode(checkbox);
+
+    // Verify that it's Off by default
+    expect(checkboxNode.textContent).toEqual('Off');
+
+    // Simulate a click and verify that it is now On
+    TestUtils.Simulate.change(
+      TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input')
+    );
+    expect(checkboxNode.textContent).toEqual('On');
+  });
+
+});
