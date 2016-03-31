e4bdb588bac9d247ba7443cb631dd6fed1518a2d
felixdrp <felixdrp@gmail.com>
2016-03-31 17:27:34 +0100

    Added more oauth test

--- test/modules/oauth.js
+++ test/modules/oauth.js
@@ -1,15 +1,47 @@
-var should = require('chai').should();
-var OAuth = require('../../build/oauth/oauth.js');
+// var should = require('chai').should();
+import { should, expect } from 'chai'
+import OAuth from '../../build/oauth/oauth.js'
 
-describe('oAuth', function () {
-  it('fails creating nameless schema', function () {
-    (function () {
-      new Schema();
-    }).should.throw();
-  });
-  it('oauth nonce default size is 42', function () {
-    var oauthobj = new OAuth.default();
-    oauthobj.getOAuthNonce().length.should.equal(42);
-  });
+describe('oAuth class', function () {
+  describe('oauth_nonce generation', () => {
+    let oauthobj = new OAuth(),
+        // test with a big string.
+        longString = 1e3;
 
+    it('oauth_nonce default size is 42', function () {
+      expect( oauthobj.getOAuthNonce() ).to.have.lengthOf(42);
+    });
+
+    it('oauth_nonce size is 10', function () {
+      expect( oauthobj.getOAuthNonce( {size: 10} ) ).to.have.lengthOf(10);
+    });
+
+    it('oauth_nonce default codification is 36', function () {
+      expect( oauthobj.getOAuthNonce( {size: longString} ) ).satisfy( nonceCode => RegExp('[0-z]{' + longString + '}').test( nonceCode ) );
+    });
+
+    it('oauth_nonce codification is binary', function () {
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 2} ) ).to.satisfy( nonceCode => RegExp('[0-1]{' + longString + '}').test( nonceCode ) );
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 2} ) ).to.not.satisfy( nonceCode => RegExp('[2-z]+').test( nonceCode ) );
+    });
+
+    it('oauth_nonce codification is octal', function () {
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 8} ) ).to.satisfy( nonceCode => RegExp('[0-7]{' + longString + '}').test( nonceCode ) );
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 8} ) ).to.not.satisfy( nonceCode => RegExp('[8-z]+').test( nonceCode ) );
+    });
+
+    it('oauth_nonce codification is hex', function () {
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 16} ) ).to.satisfy( nonceCode => RegExp('[0-f]{' + longString + '}').test( nonceCode ) );
+      expect( oauthobj.getOAuthNonce( {size: longString, codification: 16} ) ).to.not.satisfy( nonceCode => RegExp('[g-z]+').test( nonceCode ) );
+    });
+
+    it('oauth_nonce codification is not bigger than 36', function () {
+      expect(
+        () => {
+          oauthobj.getOAuthNonce( {size: longString, codification: 37} );
+        }
+      ).to.throw('toString() radix argument must be between 2 and 36');
+    });
+
+  });
 });
